1e61a2b90b9ecf661cb0f6d8edc22c3cae752bf9
Abhi <rajputabhishek677@gmail.com>
2020-05-20 08:22:08 +0530

    Check prime

---
+++ Check_Prime.cpp
@@ -0,0 +1,36 @@
+/*Take as input a number N, print "Prime" if it is prime if not Print "Not Prime".
+
+Input Format
+Constraints
+2 < N <= 1000000000
+
+Output Format
+Sample Input
+3
+Sample Output
+Prime
+Explanation
+The output is case specific*/
+
+
+#include<iostream>
+#include<math.h>
+using namespace std;
+int main()
+{
+    long n;
+    cin>>n;
+	bool flag=true;
+	for(long i=2;i<=sqrt(n);i++)
+    {
+        if(n%i==0)
+        {
+            flag=false;
+            break;
+        }
+    }
+    if(flag)
+        cout<<"Prime";
+    else
+        cout<<"Not Prime";
+}
